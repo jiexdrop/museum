73afa601ce5b33b8456b65b3fcff0b438dfe062c
jiexdrop <jorge.noogueira@gmail.com>
2019-10-09 12:01:26 +0200

    PickUp an object

---
+++ Assets/_Project/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = destination.position;
+        this.transform.parent = destination;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
